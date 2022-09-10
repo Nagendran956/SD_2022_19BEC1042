c40c85b5c91553807920a1e2b72f0579e240fbaa
Nagendran956 <67185457+Nagendran956@users.noreply.github.com>
2022-09-10 20:00:05 +0530

    Create pawn.java

---
+++ pawn.java
@@ -0,0 +1,143 @@
+public class Pawn extends Piece {
+
+    /**
+     * Creates a new Piece with a given color at (x,y)
+     * 
+     * @param isBlack Color of the piece, true if is black and white if false
+     * @param x       X coordinate of the piece
+     * @param y       Y coordinate of the piece
+     */
+    public Pawn(boolean isBlack, int x, int y) {
+        super(isBlack, x, y);
+    }
+
+    @Override
+    public String getName() {
+        return "Pawn";
+    }
+
+    @Override
+    public String getSymbol() {
+        return (isBlack) ? "P" : "p";
+    }
+
+    /**
+     * Checks if can be promoted
+     * 
+     * @return true if pawn can be promoted, false otherwise
+     */
+    public boolean canBePromoted() {
+        // If pawn is at end of board, it can promote
+        if (isBlack) {
+            if (getY() == 7) {
+                return true;
+            }
+        } else {
+            if (getY() == 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean canMove(int newX, int newY) {
+        // 1) are coordinates in range
+        if (!validCoordinates(newX, newY))
+            return false;
+
+        // Checks piece color as it can only move in one direction depending which color
+        // it is
+        if (isBlack) {
+            // no moving backwards, too far, or left/right more than 1 square
+            if (y >= newY || newY - y > 2 || Math.abs(newX - x) > 1) {
+                return false;
+            }
+            // checks movement if still in home row as it can move two squares
+            if (newY - y == 2 && y == 1 && x == newX) {
+                if ((Board.getPiece(x, y + 1) == null) && (Board.getPiece(x, y + 2) == null)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            // pawn is moving 1 square ahead in its row
+            if (newY - y == 1 && x == newX) {
+                if (Board.getPiece(x, y + 1) == null) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            // pawn is capturing a piece diagonally
+            if (newY - y == 1 && Math.abs(x - newX) == 1) {
+                if (validCoordinates(newX, newY) && Board.getPiece(newX, newY) != null) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        } else {
+            // no moving backwards, too far, or left/right more than 1 square
+            if (y <= newY || y - newY > 2 || Math.abs(newX - x) > 1) {
+                return false;
+            }
+            // checks movement if still in home row as it can move two squares
+            if (y - newY == 2 && y == 6 && x == newX) {
+                if (Board.getPiece(x, y - 1) == null && Board.getPiece(x, y - 2) == null) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            // pawn is moving 1 square ahead in its row
+            if (y - newY == 1 && x == newX) {
+                if (Board.getPiece(x, y - 1) == null) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            // pawn is capturing a piece diagonally
+            if (y - newY == 1 && Math.abs(x - newX) == 1) {
+                if (validCoordinates(newX, newY) && Board.getPiece(newX, newY) != null) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public void updateLegalMoves() {
+        legalMoves.clear();
+        if (isBlack) {
+            // vertical moves
+            if (Board.testMove(isBlack, x, y, x, y + 1))
+                legalMoves.add(new Move(x, y + 1));
+            if (Board.testMove(isBlack, x, y, x, y + 2))
+                legalMoves.add(new Move(x, y + 2));
+
+            // diagonal captures
+            if (Board.testMove(isBlack, x, y, x + 1, y + 1))
+                legalMoves.add(new Move(x + 1, y + 1));
+            if (Board.testMove(isBlack, x, y, x - 1, y + 1))
+                legalMoves.add(new Move(x - 1, y + 1));
+        } else {
+            // vertical moves
+            if (Board.testMove(isBlack, x, y, x, y - 1))
+                legalMoves.add(new Move(x, y - 1));
+            if (Board.testMove(isBlack, x, y, x, y - 2))
+                legalMoves.add(new Move(x, y - 2));
+
+            // diagonal captures
+            if (Board.testMove(isBlack, x, y, x + 1, y - 1))
+                legalMoves.add(new Move(x + 1, y - 1));
+            if (Board.testMove(isBlack, x, y, x - 1, y - 1))
+                legalMoves.add(new Move(x - 1, y - 1));
+        }
+    }
